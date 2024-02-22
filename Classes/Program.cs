85148a1e731d8ff1dbd2ec780870bee1ef5064d0
tyler davis <tyler.davis432@gmail.com>
2024-02-21 20:00:02 -0500

    completed work

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace Classes
+﻿using Microsoft.VisualBasic;
+using System.Xml.Schema;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myCar = new car();
+            myCar.make = "toyota";
+            myCar.color = "silver";
+            myCar.year = 2012;
+            myCar.model = "camery";
+
+            var Honda = new car()
+            {
+                make = "Honda",
+                model = "Accord",
+                year = 2006
+            };
+
+            var carList = new List<car>() { myCar , Honda};
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.make} {vehicle.year} {vehicle.model} {vehicle.color} ");
+            }
+            Console.WriteLine("We Got Toyotas, we got Hondas. Yup thats all we got, but they're for sale!");
         }
     }
 }
---
+++ Classes/car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class car
+    {
+        public car()
+        {
+
+        }
+        public string make { get; set; }
+        public int year { get; set; }
+        public string color { get; set; }
+        public string model { get; set; }
+
+    }
+}
